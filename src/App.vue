ecf2e644b131506951920911d1460f7520081a00
Your Name <sunilbpoojari@gmail.com>
2020-11-21 21:47:38 +0530

    Cleaned up props and other data element

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,29 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <topbar v-bind:data="data_to_topbar" ></topbar>
+    <topbar></topbar>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import topbar from './components/topbar.vue'
 export default {
   name: 'App',
   components: {
-    // HelloWorld
     topbar
   },
   data: () => ({
-    data_to_topbar : {"time_remaining ":300,
-                          "refreshed":true,
-                          "data_to_list":10,
-                          },
+
 
   }),
   mounted: function () {
   window.setInterval(() => {
-    console.log("[app][created] Checking getting api data on regular basis");
-    this.data_to_topbar.refreshed  = this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
-  }, 1000*60*5)
+    console.log("[app][created] Getting api data on regular basis ");
+    this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
+  }, 1000*60)
 },
   created: function(){
     console.log("[app][created] Checking getting api data ");
-    this.data_to_topbar.refreshed  = this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
-
+    this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
   },
 
 
@@ -39,20 +31,20 @@ export default {
   get_data_from_api: function(url){
     console.log("[app][methods][get_data_from_api] ");
     console.log("[app][method][get_data_from_api] connecting to api");
+    this.$store.dispatch('refresh_button_load')
     const baseURI = url;
     this.$http.get(baseURI)
     .then(response => {
       console.log("[app][method][get_data_from_api] success!")
       console.log(response.data);
       console.log("[app][method][get_data_from_api] updating data to component ");
-      return true
+      this.$store.dispatch('set_api_data',response.data)
     })
     .catch(error => {
       console.log("[app][method][get_data_from_api] failed!")
       console.log(error)
-      return false
+      this.$store.dispatch('error_in_api')
     })
-
     },
   }
 }
