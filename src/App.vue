148bb3492c4c37115ac4be02f5a087281c9fab3a
Your Name <sunilbpoojari@gmail.com>
2020-11-21 20:11:25 +0530

    Added vue resuource for fetching api data , method to get data

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,44 @@ export default {
   },
   data: () => ({
     data_to_topbar : {"time_remaining ":300,
-                          "refreshed":false,
+                          "refreshed":true,
                           "data_to_list":10,
                           },
 
   }),
+  mounted: function () {
+  window.setInterval(() => {
+    console.log("[app][created] Checking getting api data on regular basis");
+    this.data_to_topbar.refreshed  = this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
+  }, 1000*60*5)
+},
+  created: function(){
+    console.log("[app][created] Checking getting api data ");
+    this.data_to_topbar.refreshed  = this.get_data_from_api("https://api.coinranking.com/v1/public/coins/?limit=100")
+
+  },
+
+
+  methods:{
+  get_data_from_api: function(url){
+    console.log("[app][methods][get_data_from_api] ");
+    console.log("[app][method][get_data_from_api] connecting to api");
+    const baseURI = url;
+    this.$http.get(baseURI)
+    .then(response => {
+      console.log("[app][method][get_data_from_api] success!")
+      console.log(response.data);
+      console.log("[app][method][get_data_from_api] updating data to component ");
+      return true
+    })
+    .catch(error => {
+      console.log("[app][method][get_data_from_api] failed!")
+      console.log(error)
+      return false
+    })
+
+    },
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 
+//using  libs
+Vue.use(VueResource);
 export const bus = new Vue();
 
 new Vue({
