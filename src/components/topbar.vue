fbee3b08b8bc7227b76ae38ceec589a4aa3a9485
Your Name <sunilbpoojari@gmail.com>
2020-11-21 21:57:15 +0530

    Revert "Cleaned up props and other data element"
    This reverts commit ecf2e644b131506951920911d1460f7520081a00.

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -1,32 +1,57 @@
 <template>
   <div>
       <h1>{{tittle}}</h1>
-      <button v-on:click = "refresh_data">{{this.$store.state.button_name}}</button>
+      <p>{{data}}</p>
+      <p>{{this.$store.state.table_len}}</p>
+      <button v-on:click = "refresh_data">{{button_name}}</button>
+
   </div>
 </template>
 
 <script>
+// import { bus } from '../../main.js';
 
-export default {
-  name: 'topbar',
-  data () {
-    return {
-      tittle : "Top Crypto Currency List",
-    }
-  },
-  created:function(){
+  export default {
+    name: 'sidebar',
+    data () {
+      return {
+        tittle : "Top Crypto Currency List",
+        button_name : "Refresh Data"
+      }
+    },
+    props: [
+      "data"
+    ],
+    watch: {
+      '$props':{
+        handler: function (val, oldVal) {
+          console.log('[expense][watch] new', val)
+          console.log("[expense][watch] old",oldVal);
+        },
+        deep: true
+      }
+    },
 
-  },
-  computed: {
 
-  },
-  methods:{
-    refresh_data: function(){
-      console.log("[topbar][methods][refresh data ] sending signal to app to refresh data ");
-      console.log(this.$store.state.table_len)
-      this.$store.dispatch('get_data_from_api')
-      console.log(this.$store.state.table_len)
-      this.button_name = "Refreshing .. "
+    created:function(){
+      // bus.$on('sidebar', () => {
+      //   console.log("[sidebar.vue][created][from bus][sidebar] open sidebar");
+      //   this.drawer = true;
+      //   }
+      // );
+    },
+    computed: {
+
+    },
+    methods:{
+      refresh_data: function(){
+        console.log("[topbar][methods][refresh data ] sending signal to app to refresh data ");
+        // window.location.href = '/profile/';
+        console.log(this.$store.state.table_len)
+        this.$store.commit('set_len',40)
+        console.log(this.$store.state.table_len)
+
+        this.button_name = "Refreshing .. "
       },
     }
   }
--- src/main.js
+++ src/main.js
@@ -15,46 +15,15 @@ export const bus = new Vue();
 const store = new Vuex.Store({
   state: {
     data: {},
-    button_name : "Refresh",
-    table_len : 10,
-    error_status : false
+    refreshed : true,
+    table_len : 10
   },
   mutations: {
     set_len (state,number) {
       console.log('[main][vuex][mutation][set_len] setting data to ',number);
-    },
-    set_api_data (state,data){
-      console.log("[main][mutation][set_api_data] api call was successful  ");
-      state.data = data
-      state.button_name = "Refresh"
-      },
-    error_in_api (state){
-      console.log("[main][mutation][error_in_api] api call was unsuccessful  ");
-      state.data = {}
-      },
-    refresh_button_load (state){
-      console.log("[main][vuex][mutation][refresh_button_load]  setting to refresh  ");
-      state.button_name = "Refreshing."
-      },
+      state.table_len = number
+    }
   },
-  actions: {
-    set_len (context,number) {
-      console.log('[main][vuex][action][increment] setting data to ',number);
-      context.commit('set_len',number)
-    },
-    set_api_data (context,data){
-      console.log("[main][vuex][action][set_api_data] ")
-      context.commit('set_api_data',data)
-    },
-    error_in_api (context) {
-      console.log('[main][vuex][action][error_in_api] api call was unsuccessful ');
-      context.commit('error_in_api')
-    },
-    refresh_button_load (context){
-      console.log("[main][vuex][action][refresh_button_load]  setting to refresh  ");
-      context.commit('refresh_button_load')
-      },
-  }
 })
 
 new Vue({
