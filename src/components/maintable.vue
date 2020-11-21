0bbbd2835593eb46c324267af47b21754f3a2873
Your Name <sunilbpoojari@gmail.com>
2020-11-21 22:12:59 +0530

    File to add main table

---
+++ src/components/maintable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      my tabl
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'maintable',
+  data () {
+    return {
+
+
+    }
+  },
+  created:function(){
+
+  },
+  computed: {
+
+  },
+  methods:{
+
+    }
+  }
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
